2af33c4bb5240e578d8f3d6d51a8cb3f91983d18
Yiyio <indaghetto@gmail.com>
2017-05-01 21:41:11 +0200

    first level of Text101 almost done

--- Text101/Assets/TextController.cs
+++ Text101/Assets/TextController.cs
@@ -6,15 +6,31 @@ public class TextController : MonoBehaviour {
 
 	public Text text;
 
+	private enum States {cell, mirror, sheets_0, lock_0, sheets_1, lock_1, freedom};
+	private States myState;
+
 	// Use this for initialization
 	void Start () {
-		text.text = "Hello world";
+		myState = States.cell;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Space)){
-			text.text = "Space key pressed";
-		}
+		print (myState);
+		if(myState == States.cell){
+			state_cell();
+		} else if (myState == States.sheets_0
+	}
+
+	void state_cell(){
+		text.text = "You wake up feeling dizzy and disoriented. The air is damp, and the little light comes " +
+			"into the room from a very small window at the top of your cell. " +
+				"Looking around, you don't see many things in your cell: a wall mirror, dirty sheets on the bed, " + 
+				"and of course, a locked door.\n\n" +
+	 			"Press S to view the Sheets, M to view the Mirror, L to view the Lock";
+
+			if(Input.GetKeyDown(KeyCode.S)){
+			myState = States.sheets_0;
+			}
 	}
 }
