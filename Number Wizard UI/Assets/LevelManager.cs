53944590dc213920120c0692369d40d6c8b98aff
Yiyio <indaghetto@gmail.com>
2018-03-26 23:31:56 +0200

    main menu for Text101

---
+++ Number Wizard UI/Assets/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+public void LoadLevel(string name){
+		Debug.Log("Level load requested for: " + name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("so quitting");
+}
+
+}
+
+
