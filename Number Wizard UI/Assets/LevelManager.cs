ad45694d3f0a9caf21985465d8deb7fd4cf773fe
Yiyio <indaghetto@gmail.com>
2018-04-02 18:34:38 +0200

    number wizard UI now using random guesses

--- Number Wizard UI/Assets/LevelManager.cs
+++ Number Wizard UI/Assets/LevelManager.cs
@@ -5,12 +5,14 @@ public class LevelManager : MonoBehaviour {
 
 public void LoadLevel(string name){
 		Debug.Log("Level load requested for: " + name);
+		Application.LoadLevel(name);
 	}
 
 	public void QuitRequest(){
 		Debug.Log ("so quitting");
+		Application.Quit();
 }
-
+	
 }
 
 
